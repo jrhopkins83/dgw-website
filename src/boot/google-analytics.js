983959ea7ae116406c690b37a3d3dded17e7f0c2
Roger Hopkins <jrhopkins83@gmail.com>
2021-02-19 15:26:45 -0600

    Deleted sessionID

--- src/boot/google-analytics.js
+++ src/boot/google-analytics.js
@@ -3,6 +3,6 @@ import ga from 'src/analytics.js'
 export default ({ router }) => {
   router.afterEach((to, from) => {
     // eslint-disable-next-line no-undef
-    ga.logPage(to.path, to.name, sessionId)
+    ga.logPage(to.path, to.name)
   })
 }
